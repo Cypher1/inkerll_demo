6903c56300636069b6321fb98938126ee93f508c
J Pratt <jp10010101010000@gmail.com>
2023-02-28 09:55:43 +1100

    Demo?

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use inkwell::OptimizationLevel;
+use inkwell::builder::Builder;
+use inkwell::context::Context;
+use inkwell::execution_engine::{ExecutionEngine, JitFunction};
+use inkwell::module::Module;
+use inkwell::targets::{CodeModel, RelocMode, FileType, Target, TargetTriple, InitializationConfig};
+use std::error::Error;
+use std::path::Path;
+
+/// Convenience type alias for the `sum` function.
+///
+/// Calling this is innately `unsafe` because there's no guarantee it doesn't
+/// do `unsafe` operations internally.
+type SumFunc = unsafe extern "C" fn(u64, u64, u64) -> u64;
+
+struct CodeGen<'ctx> {
+    context: &'ctx Context,
+    module: Module<'ctx>,
+    builder: Builder<'ctx>,
+    execution_engine: ExecutionEngine<'ctx>,
+}
+
+impl<'ctx> CodeGen<'ctx> {
+    fn jit_compile_sum(&self) -> Option<JitFunction<SumFunc>> {
+        let i64_type = self.context.i64_type();
+        let fn_type = i64_type.fn_type(&[i64_type.into(), i64_type.into(), i64_type.into()], false);
+        let function = self.module.add_function("sum", fn_type, None);
+        let basic_block = self.context.append_basic_block(function, "entry");
+
+        self.builder.position_at_end(basic_block);
+
+        let x = function.get_nth_param(0)?.into_int_value();
+        let y = function.get_nth_param(1)?.into_int_value();
+        let z = function.get_nth_param(2)?.into_int_value();
+
+        let sum = self.builder.build_int_add(x, y, "sum");
+        let sum = self.builder.build_int_add(sum, z, "sum");
+
+        self.builder.build_return(Some(&sum));
+
+        unsafe { self.execution_engine.get_function("sum").ok() }
+    }
+}
+
+fn write_machine() {
+    Target::initialize_x86(&InitializationConfig::default());
+
+    let opt = OptimizationLevel::Default;
+    let reloc = RelocMode::Default;
+    let model = CodeModel::Default;
+    // let path = Path::new("/tmp/some/path/main.asm");
+    let path = Path::new("./main.asm");
+    let target = Target::from_name("x86-64").unwrap();
+    let target_machine = target.create_target_machine(
+        &TargetTriple::create("x86_64-pc-linux-gnu"),
+        "x86-64",
+        "+avx2",
+        opt,
+        reloc,
+        model
+    )
+    .unwrap();
+
+    let context = Context::create();
+    let module = context.create_module("my_module");
+    let void_type = context.void_type();
+    let fn_type = void_type.fn_type(&[], false);
+
+    module.add_function("my_fn", fn_type, None);
+
+    assert!(target_machine.write_to_file(&module, FileType::Object, &path).is_ok());
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let context = Context::create();
+    let module = context.create_module("sum");
+    let execution_engine = module.create_jit_execution_engine(OptimizationLevel::None)?;
+    let codegen = CodeGen {
+        context: &context,
+        module,
+        builder: context.create_builder(),
+        execution_engine,
+    };
+
+    let sum = codegen.jit_compile_sum().ok_or("Unable to JIT compile `sum`")?;
+
+    let x = 1u64;
+    let y = 2u64;
+    let z = 3u64;
+
+    let res = unsafe {
+        sum.call(x, y, z) // The unsafe thing is to call 'sum'.
+    };
+
+    println!("{} + {} + {} = {}", x, y, z, res);
+    assert_eq!(res, x + y + z);
+
+    dbg!(codegen.module.write_bitcode_to_path(Path::new("./a.out")));
+
+    write_machine();
+
+    Ok(())
+}
